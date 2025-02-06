40119645355cfa43d01967cb5b18d718da1f75ae
Jemal <jamalsamba@gmail.com>
2025-02-06 22:16:44 +0000

    added roadmap section

---
+++ src/components/home/roadmapsolving.data.ts
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    id: 1,
+    title: 'Strategy & Consulting',
+    description: 'Reviewing your current capabilities and business goals to create your digital transformation and AI implementation plan.',
+  },
+  {
+    id: 2,
+    title: 'Proof of Value',
+    description: 'Rapidly develop a proof-of-concept (PoC) to validate and confirm the value of AI-driven business cases.',
+  },
+  {
+    id: 3,
+    title: 'Minimum Viable Product',
+    description: 'Building an MVP solution to make it available for your customers and allow you to test your product idea and gather customer feedback.',
+  },
+  {
+    id: 4,
+    title: 'Maintenance',
+    description: 'Improvements to ensure product performance, enhance user experience and continuously keep up with the changing environment.',
+  },
+]
