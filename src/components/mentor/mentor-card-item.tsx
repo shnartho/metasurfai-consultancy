54f7c4a262ae7ac6667fcf273417e27abafe1359
Jemal <jamalsamba@gmail.com>
2025-02-06 22:18:14 +0000

    modifed our ai teachers

--- src/components/mentor/mentor-card-item.tsx
+++ src/components/mentor/mentor-card-item.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import Image from 'next/image'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 import { Mentor } from '@/interfaces/mentor'
 
@@ -13,47 +14,41 @@ const MentorCardItem: FC<Props> = ({ item }) => {
   return (
     <Box
       sx={{
-        px: 1.5,
-        py: 5,
+        display: 'flex',
+        alignItems: 'center',
+        p: 2,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 4,
+        border: '1px solid white',
+        mb: 2,
       }}
     >
       <Box
         sx={{
-          p: 2,
-          backgroundColor: 'background.paper',
-          borderRadius: 4,
-          transition: (theme) => theme.transitions.create(['box-shadow']),
-          '&:hover': {
-            boxShadow: 2,
-          },
+          width: 100,
+          height: 100,
+          borderRadius: '50%',
+          overflow: 'hidden',
+          mr: 2,
         }}
       >
-        <Box
-          sx={{
-            lineHeight: 0,
-            overflow: 'hidden',
-            borderRadius: 3,
-            height: 200,
-            mb: 2,
-          }}
+        <Image src={item.photo} width={100} height={100} alt={'Mentor ' + item.name} />
+      </Box>
+      <Box sx={{ flex: 1 }}>
+        <Typography component="h2" variant="h5" sx={{ fontSize: '1.4rem' }}>
+          {item.name}
+        </Typography>
+        <Typography sx={{ color: 'text.secondary' }}>{item.category}</Typography>
+        <Button
+          variant="contained"
+          sx={{ mt: 1, backgroundColor: 'red', color: 'white' }}
+          href={item.contact}
         >
-          <Image src={item.photo as string} width={570} height={427} alt={'Mentor ' + item.id} />
-        </Box>
-        <Box sx={{ mb: 2 }}>
-          <Typography component="h2" variant="h4" sx={{ fontSize: '1.4rem' }}>
-            {item.name}
-          </Typography>
-          <Typography sx={{ mb: 2, color: 'text.secondary' }}>{item.category}</Typography>
-          <Typography sx={{ mb: 2, color: 'text.secondary' }} variant="subtitle1">
-            {item.description}
-          </Typography>
-          <Box sx={{ '& img': { height: 26 } }}>
-            {/* eslint-disable-next-line */}
-            <img src={item.company?.logo} alt={item.company?.name + ' logo'} />
-          </Box>
-        </Box>
+          Contact {item.name}
+        </Button>
       </Box>
     </Box>
   )
 }
+
 export default MentorCardItem
--- src/interfaces/mentor.ts
+++ src/interfaces/mentor.ts
@@ -1,10 +1,8 @@
 import type { User } from './user'
 
 export interface Mentor extends Omit<User, 'professional'> {
-  description?: string
+  photo: string
+  name: string
   category: string
-  company?: {
-    name: string
-    logo: string
-  }
+  contact: string
 }
