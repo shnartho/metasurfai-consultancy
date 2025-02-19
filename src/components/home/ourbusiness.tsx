5bfbeba850eff0b2721e6a58351286f64211c5a3
Jemal <jamalsamba@gmail.com>
2025-02-19 19:07:03 +0000

    changed our business icons colors, box shadow and text color

--- src/components/home/ourbusiness.tsx
+++ src/components/home/ourbusiness.tsx
@@ -6,7 +6,7 @@ import { data } from './ourbusiness.data'
 const OurBusiness: FC = () => {
   return (
     <Box sx={{ p: 4, backgroundColor: 'white', position: 'relative', overflow: 'hidden' }}>
-      <Typography variant="h1" component="h1" sx={{ mb: 2, textAlign: 'center' }}>
+      <Typography variant="h1" component="h1" sx={{ mb: 2, textAlign: 'center', color: '#0066FF' }}>
         Consulting Services For Businesses
       </Typography>
       <Typography variant="body1" sx={{ mb: 4, textAlign: 'center' }}>
@@ -20,7 +20,7 @@ const OurBusiness: FC = () => {
             sx={{
               backgroundColor: 'white',
               borderRadius: 2,
-              boxShadow: '0 2px 2px #0B87D7', // Blue shadow
+              boxShadow: '0 2px 2px #212121', // Blue shadow
               p: 3,
               width: '40%',
               maxWidth: '250px',
@@ -33,10 +33,10 @@ const OurBusiness: FC = () => {
             <Box sx={{ mb: 2 }}>
               <img src={item.icon} alt={item.title} style={{ width: '64px', height: '64px' }} />
             </Box>
-            <Typography variant="h6" component="h2" sx={{ mb: 1, color: 'skyblue' }}>
+            <Typography variant="h6" component="h2" sx={{ mb: 1, color: '#212121' }}>
               {item.title}
             </Typography>
-            <Typography variant="body2">
+            <Typography variant="body2" sx={{ color: '#212121' }}>
               {item.description}
             </Typography>
           </Box>
